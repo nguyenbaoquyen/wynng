b4470a6a928481465a51f7b09feae3647738ec56
nguyenbaoquyen <163015871+nguyenbaoquyen@users.noreply.github.com>
2024-03-11 22:17:07 +0700

    Add files via upload

---
+++ tính số tuổi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int nNamSinh(0);
+	int nYear(2024);
+	cout << "Nhap vao nam sinh cua ban: ";
+	cin >> nNamSinh;
+
+	cout << "Ban sinh nam: " << nNamSinh << endl;
+	cout << "Ban co so tuoi: " << 2024 - nNamSinh << endl;
+}
+
